71d3431e67d17163bbe44abdd7f14079644c6dc3
Vandan Boghara <159246115+Vandanx712@users.noreply.github.com>
2026-03-02 13:12:41 +0530

    add new package

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from 'cors'
 
 dotenv.config();
 
 const port = process.env.PORT;
 
-const app = express();
+const app = express(cors());
 
 app.use(express.json());
 
