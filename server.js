2caef9c7c0ed354049c5f824025e3959aea220b8
Vandan Boghara <159246115+Vandanx712@users.noreply.github.com>
2026-03-02 13:01:22 +0530

    Init Node/Express project scaffold
    Add initial project scaffold: .gitignore, package.json (declares name, version, scripts and dependencies: express, dotenv, nodemon) and server.js implementing a minimal Express app that loads environment variables, defines a root route, and starts the server. This sets up the base for development and local reloading.

---
+++ server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const port = process.env.PORT;
+
+const app = express();
+
+app.use(express.json());
+
+app.post("/api/track", (req, res) => {
+  const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+
+  const userAgent = req.headers["user-agent"];
+
+  const event = {
+    ...req.body,
+    ip,
+    userAgent,
+    receivedAt: new Date(),
+  };
+
+  console.log("📦 EVENT RECEIVED:");
+  console.log(JSON.stringify(event, null, 2));
+
+  res.status(200).json({ success: true });
+});
+
+app.listen(port, () => console.log(`app run on ${port}`));
